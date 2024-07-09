d1ce13af52a019600bc8419399fc565ab27a1be6
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2024-07-09 23:09:56 +0800

    fetch script

--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -2,12 +2,31 @@
   <v-container class="fill-height" fluid>
     <v-row>
       <v-col cols="12">
-        <v-data-table :items="items"></v-data-table>
+        <v-data-table :items="students"></v-data-table>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
-<script setup>
+<script>
+import axios from 'axios';
+export default {
+  name: 'students',
+  data() {
+    return {
+      students: [],
+    }
+  },
+  mounted() {
+    this.getStudents();
+  },
+  methods: {
+    getStudents() {
+      axios.get('http://localhost/crud-vue-php/src/crud/fetch.php').then(res => {
+        this.students = res.data;
+      });
+    }
+  }
 
+}
 </script>
