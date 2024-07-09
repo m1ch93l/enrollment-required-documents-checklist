6b59f3beeb9e7312064a9a77141fe2bb7a37bcce
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2024-07-10 00:09:29 +0800

    add header

--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -2,7 +2,8 @@
   <v-container class="fill-height" fluid>
     <v-row>
       <v-col cols="12">
-        <v-data-table :items="students"></v-data-table>
+        <v-data-table :headers="headers" :items="students">
+        </v-data-table>
       </v-col>
     </v-row>
   </v-container>
@@ -10,11 +11,22 @@
 
 <script>
 import axios from 'axios';
+
 export default {
-  name: 'students',
   data() {
     return {
       students: [],
+      headers: [
+        { title: '#', value: 'id' },
+        { title: 'Name', value: 'name' },
+        { title: 'Original copy of PSA/Birth Certificate', value: 'o_psa' },
+        { title: 'Original copy of F138', value: 'o_f138' },
+        { title: 'Original copy of F137 (School-to-school)', value: 'o_f137' },
+        { title: 'Photocopy of Good Moral Certificate', value: 'p_gmc' },
+        { title: 'Photocopy of Certificate of Completion', value: 'p_coc' },
+        { title: 'Pictures (2x2 & 1x1)', value: 'pic' },
+        { title: 'ESC (only for Private School)', value: 'esc' },
+      ]
     }
   },
   mounted() {
