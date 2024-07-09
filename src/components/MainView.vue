80a4717c96c00ca0948dac6437231404e66a705e
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2024-07-09 21:57:57 +0800

    add main table

--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -1,9 +1,13 @@
 <template>
   <v-container class="fill-height" fluid>
-    
+    <v-row>
+      <v-col cols="12">
+        <v-data-table :items="items"></v-data-table>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script setup>
-  //
+
 </script>
