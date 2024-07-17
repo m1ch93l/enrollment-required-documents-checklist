70d27b7b2ba405b2f92dfe9ca72fea0b5188613b
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2024-07-17 09:36:43 +0800

    sortable true

--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -3,6 +3,7 @@
     <v-row>
       <v-col cols="12">
         <v-data-table :headers="headers" :items="students">
+
         </v-data-table>
       </v-col>
     </v-row>
@@ -17,16 +18,30 @@ export default {
     return {
       students: [],
       headers: [
-        { title: '#', value: 'id' },
-        { title: 'Name', value: 'name', align: 'center' },
-        { title: 'Original copy of PSA/Birth Certificate', value: 'o_psa', align: 'center' },
-        { title: 'Original copy of F138', value: 'o_f138', align: 'center' },
-        { title: 'Original copy of F137 (School-to-school)', value: 'o_f137', align: 'center' },
-        { title: 'Photocopy of Good Moral Certificate', value: 'p_gmc', align: 'center' },
-        { title: 'Photocopy of Certificate of Completion', value: 'p_coc', align: 'center' },
-        { title: 'Pictures (2x2 & 1x1)', value: 'pic', align: 'center' },
-        { title: 'ESC (only for Private School)', value: 'esc', align: 'center' },
-      ]
+        { title: '#', value: 'id', sortable: false },
+        { title: 'Name', value: 'name', align: 'center', sortable: true },
+        { title: 'Original copy of PSA/Birth Certificate', value: 'o_psa', align: 'center', sortable: false },
+        { title: 'Original copy of F138', value: 'o_f138', align: 'center', sortable: false },
+        { title: 'Original copy of F137 (School-to-school)', value: 'o_f137', align: 'center', sortable: false },
+        { title: 'Photocopy of Good Moral Certificate', value: 'p_gmc', align: 'center', sortable: false },
+        { title: 'Photocopy of Certificate of Completion', value: 'p_coc', align: 'center', sortable: false },
+        { title: 'Pictures (2x2 & 1x1)', value: 'pic', align: 'center', sortable: false },
+        { title: 'ESC (only for Private School)', value: 'esc', align: 'center', sortable: false },
+      ],
+      items: [],
+      editItem: null,
+      editMode: false,
+      fields: [
+        { name: 'id', label: '#', type: 'number' },
+        { name: 'name', label: 'Name', type: 'text' },
+        { name: 'o_psa', label: 'Original copy of PSA/Birth Certificate', type: 'select', options: ['Yes', 'No'] },
+        { name: 'o_f138', label: 'Original copy of F138', type: 'select', options: ['Yes', 'No'] },
+        { name: 'o_f137', label: 'Original copy of F137 (School-to-school)', type: 'select', options: ['Yes', 'No'] },
+        { name: 'p_gmc', label: 'Photocopy of Good Moral Certificate', type: 'select', options: ['Yes', 'No'] },
+        { name: 'p_coc', label: 'Photocopy of Certificate of Completion', type: 'select', options: ['Yes', 'No'] },
+        { name: 'pic', label: 'Pictures (2x2 & 1x1)', type: 'select', options: ['Yes', 'No'] },
+        { name: 'esc', label: 'ESC (only for Private School)', type: 'select', options: ['Yes', 'No'] },
+      ],
     }
   },
   mounted() {
